a9048663e752595960a5bb8a3f396c4abeba9ebf
pratikb99 <50937293+pratikb99@users.noreply.github.com>
2020-06-22 08:32:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config(object):
     # The bot-token which you can get from @Botfather
     BOT_TOKEN = "1030538336:AAHB1fTBooMZ3hxlnDK9oegbT_uCXsK1mkM"
     # There is no measure to limit who can use this bot, so add userids of users authorized to use this bot
-    AUTH_USERS = []
+    AUTH_USERS = [693236796]
     # Add numbers below who shouldn't be bombed ever
     NO_BOMB_NUMS = []
     # Add userids below of users who should have sudo authority over bot, i.e., have no bombing limits
-    GOD_USERS = []
+    GOD_USERS = [693236796]
