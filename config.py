219b180ff2ba38d514ab8d2e456d7dfdc957ceca
pratikb99 <50937293+pratikb99@users.noreply.github.com>
2020-06-22 08:04:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config(object):
     # The bot-token which you can get from @Botfather
-    BOT_TOKEN = ""
+    BOT_TOKEN = "1030538336:AAHB1fTBooMZ3hxlnDK9oegbT_uCXsK1mkM"
     # There is no measure to limit who can use this bot, so add userids of users authorized to use this bot
     AUTH_USERS = []
     # Add numbers below who shouldn't be bombed ever
